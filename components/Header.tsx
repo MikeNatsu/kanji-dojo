bd1c6dc282dfd8cfcea69cf1331c80e56c0d36a1
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-13 00:23:41 -0400

    style: centering header title

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
-	return <header>
-    <h1>Kanji Dojo</h1>
-  </header>;
+	return (
+		<header>
+			<h1 className="text-center">Kanji Dojo</h1>
+		</header>
+	);
 };
 
 export default Header;
