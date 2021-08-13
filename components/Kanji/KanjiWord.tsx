b0d7e4edca5587033f22fc55f09195c87b778304
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-13 02:21:56 -0400

    fix: empty fields

--- components/Kanji/KanjiWord.tsx
+++ components/Kanji/KanjiWord.tsx
@@ -8,20 +8,28 @@ const KanjiWordComponent = ({ kanjiWord }: { kanjiWord: KanjiWordType }) => {
 				<span className="badge bg-secondary">{kanjiWord.kanji}</span> -{' '}
 				{kanjiWord.meanings[0]}
 			</h1>
-			<div className="d-flex border p-5">
+
+			<div className="d-flex p-5">
 				<ul className="list-group">
-					<li className="list-group-item">
-						<b>Grade: </b>
-						{kanjiWord.grade}
-					</li>
-					<li className="list-group-item">
-						<b>Unicode: </b>
-						{kanjiWord.unicode}
-					</li>
-					<li className="list-group-item">
-						<b>Meanings: </b>
-						{kanjiWord.meanings.join(', ')}
-					</li>
+					{kanjiWord.grade && (
+						<li className="list-group-item">
+							<b>Grade: </b>
+							{kanjiWord.grade}
+						</li>
+					)}
+					{kanjiWord.unicode && (
+						<li className="list-group-item">
+							<b>Unicode: </b>
+							{kanjiWord.unicode}
+						</li>
+					)}
+
+					{kanjiWord.meanings && (
+						<li className="list-group-item">
+							<b>Meanings: </b>
+							{kanjiWord.meanings.join(', ')}
+						</li>
+					)}
 				</ul>
 			</div>
 		</div>
