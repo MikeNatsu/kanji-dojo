497f675f3a47446327da578393eb2d8b3a50ba52
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-13 02:21:01 -0400

    Add: api and dictionary links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,14 +3,16 @@ import Link from 'next/link';
 const Nav = () => {
 	return (
 		<nav>
-			<ul>
-				<Link href="/kanji"></Link>
+			<ul className="d-flex justify-content-center m-2 p-2">
+				<Link href="/kanji" replace>
+					<a className="mx-3">Dictionary</a>
+				</Link>
+				<a href="https://kanjiapi.dev/" target="_blank" className="mx-3">
+					API
+				</a>
 			</ul>
 		</nav>
 	);
 };
-
-// BRB :v 
-
 
 export default Nav;
