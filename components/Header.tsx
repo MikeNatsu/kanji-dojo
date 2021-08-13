bd6133089ddd8bbcc3619cbabe82fb679ad88160
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-13 02:21:28 -0400

    feat: nav in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
+import Nav from './Nav';
+
 const Header = () => {
 	return (
-		<header>
-			<h1 className="text-center">Kanji Dojo</h1>
+		<header className="d-flex">
+			<h1 className="flex-grow-1">Kanji Dojo</h1>
+			<Nav />
 		</header>
 	);
 };
