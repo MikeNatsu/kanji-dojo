63e84efb42dc7c7fd1915883e42da14c3e5e4cd4
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-13 23:11:35 -0400

    fix: KanjiBox structure

--- components/Kanji/KanjiBox.tsx
+++ components/Kanji/KanjiBox.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import kanjiStyles from '../../styles/Kanji.module.scss';
 import Link from 'next/link';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import PopoverComponent from '../Popover';
+import { KanjiWordType } from '../../pages/kanji';
 
 const KanjiBox = ({ kanji }: { kanji: string }) => {
+	const [kanjiWord, setKanjiWord] = useState<KanjiWordType>();
+
+	const getKanjiInfo = async () => {
+		const res = await fetch(
+			`https://kanjiapi.dev/v1/kanji/${encodeURIComponent(kanji)}`
+		);
+		const kanjiWordRes = await res.json();
+		setKanjiWord(await kanjiWordRes);
+	};
+
+	useEffect(() => {
+		getKanjiInfo();
+	}, []);
+
 	return (
-		<Link href={`kanji/${kanji}`}>
-			<div className="bg-secondary p-4 kanjiWord">
-				<span className={kanjiStyles.kanjiWord}>{kanji}</span>
+		<div>
+			<div className="d-flex m-4">
+				<div className="py-2 px-4 rounded dark bg-secondary text-decoration-none symbolKanji">
+					<Link href={`kanji/${kanji}`}>
+						<a  >{kanji}</a>
+					</Link>
+				</div>
+				<div className="flex-grow-1 fs-4 text-capitalize text-center">
+					{kanjiWord?.meanings[0]}
+				</div>
 			</div>
-		</Link>
+			<hr />
+		</div>
 	);
 };
 
