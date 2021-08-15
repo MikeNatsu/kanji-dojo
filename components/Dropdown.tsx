145f242cc8a6d9d62b3a4cd5d1a7792aa6ebb44a
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-15 00:50:28 -0400

    feat: activate once clicked

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
 export type DropdownType = {
 	title: string;
 	items: string[];
 	setter?: (keyword: string) => void;
+	activated?: string;
+	defaultItem?: string;
 };
 
-const DropdownComponent = ({ title, items, setter }: DropdownType) => {
+const DropdownComponent = ({
+	title,
+	items,
+	setter,
+	defaultItem,
+}: DropdownType) => {
+	const [activated, setActivated] = useState<string | undefined>(defaultItem);
 	return (
-		<DropdownButton title={title} className="text-capitalize" variant="secondary">
+		<DropdownButton
+			title={title}
+			className="text-capitalize"
+			variant="secondary"
+		>
 			{items.map((item) => (
 				<Dropdown.Item
 					className="text-capitalize"
 					onClick={() => {
 						if (setter) {
 							setter(item);
+							setActivated(item);
 						}
 					}}
+					active={activated === item}
 				>
 					{item}
 				</Dropdown.Item>
