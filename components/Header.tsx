c97936af551c1920ad8a0538e0bf4041895ff551
MikeNatsu <michaelgonzalezsantamaria@gmail.com>
2021-08-15 00:50:44 -0400

    feat: clickable header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 import Nav from './Nav';
 
 const Header = () => {
 	return (
-		<header className="d-flex">
-			<h1 className="flex-grow-1 px-3">Kanji Dojo</h1>
+		<header className="header d-flex">
+			<Link href="/" replace>
+				<h1 className="flex-grow-1 px-3 ">Kanji Dojo</h1>
+			</Link>
 			<Nav />
 		</header>
 	);
